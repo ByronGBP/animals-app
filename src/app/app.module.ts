2f7fede519925d640821f5c0df33c459f5e1ca89
Luis Amez <luis.amez.vitoria@gmail.com>
2018-02-21 18:34:20 +0100

    routing lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { AnotherOneComponent } from './pages/another-one/another-one.component';
+import { AnimalPageComponent } from './pages/animal-page/animal-page.component';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
 import { AnimalCardComponent } from './components/animal-card/animal-card.component';
 import { AnimalListComponent } from './components/animal-list/animal-list.component';
 
@@ -13,18 +15,27 @@ import { AnyService } from './services/any.service';
 
 import { KebabCasePipe } from './pipes/kebab-case.pipe';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: HomepageComponent },
+  { path: 'another-one', component: AnotherOneComponent },
+  { path: 'animal/:id', component: AnimalPageComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent,
     HomepageComponent,
-    KebabCasePipe,
+    AnotherOneComponent,
+    AnimalPageComponent,
+    AppComponent,
     AnimalCardComponent,
     AnimalListComponent,
-    AnotherOneComponent
+    KebabCasePipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [AnyService],
   bootstrap: [AppComponent]
